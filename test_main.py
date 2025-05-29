f038c2f784c5a1b910f711f6edb56c4f32bf131d
Mazen05 <78726514+Mazen05@users.noreply.github.com>
2025-05-29 18:57:27 +0200

    test_main

---
+++ test_main.py
@@ -0,0 +1,30 @@
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_predict_program():
+    payload = {
+        "age": 22,
+        "sex": "M",
+        "poids": 70,
+        "taille": 180,
+        "objectif": "endurance",
+        "historique_sportif": "débutant"
+    }
+    response = client.post("/predict_program", json=payload)
+    assert response.status_code == 200
+    assert "programme_recommande" in response.json()
+    assert "details" in response.json()
+
+def test_predict_perf():
+    payload = {
+        "nb_squats": 10,
+        "nb_bench_press": 10,
+        "heures_sommeil": 8,
+        "qualite_nutrition": 80
+    }
+    response = client.post("/predict_perf", json=payload)
+    assert response.status_code == 200
+    assert "progression_estimee" in response.json()
